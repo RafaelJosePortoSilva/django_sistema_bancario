8de64229d7200777d031076594f6c67e2589e7fc
RafaelJosePortoSilva <rafaelporto.dev@gmail.com>
2024-05-23 10:59:12 -0300

    Começando a configurar os models

--- sistema_bancario_app/models.py
+++ sistema_bancario_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Conta(models.Model):
+
+    saldo = models.DecimalField(
+        max_digits=16,
+        decimal_places=2
+    )
+
+class ContaCorrente(models.Model):
+    ...
+
+class Cliente(models.Model):
+    ...
+
+class PessoaFisica(models.Model):
+    ...
+
+class Historico(models.Model):
+    ...

c0d91aefb9866e5c90f3800be940a7d99d47edba
RafaelJosePortoSilva <rafaelporto.dev@gmail.com>
2024-05-23 13:08:16 -0300

    Terminada primeira versao da tabela de clientes

--- sistema_bancario_app/models.py
+++ sistema_bancario_app/models.py
@@ -5,7 +5,27 @@ class Conta(models.Model):
 
     saldo = models.DecimalField(
         max_digits=16,
-        decimal_places=2
+        decimal_places=2,
+        default=0
+    )
+
+    numero = models.IntegerField(
+        primary_key=True,
+        unique=True
+    )
+
+    agencia = models.IntegerField(
+
+    )
+
+    cliente = models.ForeignKey(
+        'Cliente',
+        on_delete=models.CASCADE
+    )
+
+    historico = models.ForeignKey(
+        'Historico',
+        on_delete=models.CASCADE
     )
 
 class ContaCorrente(models.Model):
